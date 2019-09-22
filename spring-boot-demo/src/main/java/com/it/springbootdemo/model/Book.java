79c3b447ff9ad71d80e3bf71b8125894649276c0
liubingyu132 <1072484871@qq.com>
2019-09-22 19:46:38 +0800

    Update Book.java

--- spring-boot-demo/src/main/java/com/it/springbootdemo/model/Book.java
+++ spring-boot-demo/src/main/java/com/it/springbootdemo/model/Book.java
@@ -5,22 +5,43 @@ public class Book {
     private String ISBN;
     private String price;
     private String location;
-    private String bookname;
+    private String bookName;
     private String author;
     private String press;
-    public Book(int BookId,String ISBN,String price,String location,String bookname,String author,String press)
-    {
-        this.BookId=BookId;
-        this.ISBN=ISBN;
-        this.price=price;
-        this.location=location;
-        this.bookname=bookname;
-        this.author=author;
-        this.press=press;
+    private String description;
+    private String publishYear;
+    private String textLanguage;
+    private int copyNumbers;
+
+    public Book(int bookId, String ISBN, String price, String location, String bookname, String author,
+                String press, String description, String publishYear, String textLanguage, int copyNumbers) {
+        this.BookId = bookId;
+        this.ISBN = ISBN;
+        this.price = price;
+        this.location = location;
+        this.bookName = bookname;
+        this.author = author;
+        this.press = press;
+        this.description = description;
+        this.publishYear = publishYear;
+        this.textLanguage = textLanguage;
+        this.copyNumbers = copyNumbers;
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        return "Book{" +
+                "BookId=" + BookId +
+                ", ISBN='" + ISBN + '\'' +
+                ", price='" + price + '\'' +
+                ", location='" + location + '\'' +
+                ", bookName='" + bookName + '\'' +
+                ", author='" + author + '\'' +
+                ", press='" + press + '\'' +
+                ", description='" + description + '\'' +
+                ", publishYear='" + publishYear + '\'' +
+                ", textLanguage='" + textLanguage + '\'' +
+                ", copyNumbers=" + copyNumbers +
+                '}';
     }
 }
