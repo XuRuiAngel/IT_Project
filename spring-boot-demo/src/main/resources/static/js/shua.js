b5b1124e25e0f6d93033577e0edeff30fe48f443
mryu12 <55574506+mryu12@users.noreply.github.com>
2019-09-29 16:02:38 +0800

    searchBook

--- spring-boot-demo/src/main/resources/static/js/shua.js
+++ spring-boot-demo/src/main/resources/static/js/shua.js
@@ -10,8 +10,15 @@ function searchBook() {
             data:{},
             dataType: "text",
             success: function (data) {
-                alert(key);
-                alert(data);
+                pager.init(1);
+                pager.setLayout('pagerDiv', data, 100, function () {
+                    $('#tbody').html('');
+                    for (var i = pager.start() - 1; i < pager.end(); i++) {
+                        if (data[i]) {
+                            $('#tbody').append('<tr><td>' + data[i].BookId + '</td>' + '<td>'+ data[i].ISBN+ '</td>' + '<td>'+data[i].Price + '</td>'+ '<td>'+data[i].Location + '</td>'+  '<td>'+data[i].BookName + '</td>'+  '</td>'+data[i].Author + '</td>'+ '<td>'+data[i].Press + '</td>'+'<td>'+data[i].Author + '</td>'+'<td>'+data[i].Description + '</td>'+'<td>'+data[i].PublishYear + '</td>'+'<td>'+data[i].TextLanguage + '</td>'+'<td>'+data[i].CopyNumbers +'</td></tr>');
+                        }
+                    }
+                });
             }
 
         })
