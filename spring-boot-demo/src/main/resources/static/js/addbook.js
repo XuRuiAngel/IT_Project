ccd99ebf99a4e1907d7c56eb6f31e2ae66c156c0
mryu12 <55574506+mryu12@users.noreply.github.com>
2019-10-13 20:54:19 +0800

    addbook full version 2.0

--- spring-boot-demo/src/main/resources/static/js/addbook.js
+++ spring-boot-demo/src/main/resources/static/js/addbook.js
@@ -1,29 +1,26 @@
 function addBook1(){
     var num=$("#cpn").val();
-    for(var i=0;i<num;i++){
-        addBook();
-    }
-    alert("success");
-}
-function addBook() {
     var bn=$("#bname").val();
     var ba=$("#bauthor").val();
     var bp1=$("#bpress").val();
     var bp=$("#bprice").val();
     var bl=$("#floor option:selected").val()+$("#Shelf option:selected").val();
+    tosu();
+    for(var i=0;i<num;i++){
+        $.ajax({
+            async : false,
+            type : "GET",
+            url : "/insertBookWithoutISBN?price="+bp+"&location="+bl+"&bookname="+bn+"&author="+ba+"&press="+bp1,
+            data :{},
+            success : function(data) {
+
+            },
+            error : function(data) {
+
+            }
+        });
+    }
 
 
-    $.ajax({
-        async : false,
-        type : "GET",
-        url : "/insertBookWithoutISBN?price="+bp+"&location="+bl+"&bookname="+bn+"&author="+ba+"&press="+bp1,
-        data :{},
-        success : function(data) {
 
-        },
-        error : function(data) {
-
-        }
-    });
-
-};
+}
--- spring-boot-demo/src/main/resources/static/js/addbookbyisbn.js
+++ spring-boot-demo/src/main/resources/static/js/addbookbyisbn.js
@@ -1,29 +1,27 @@
 function addBookByISBN1(){
-    var num=$("#cpn").val();
-    for(var i=0;i<num;i++){
-        addBookByISBN();
-    }
-    alert("success");
-}
-function addBookByISBN() {
-	var isbn=$('#ISBN').val();
+    var isbn=$('#ISBN').val();
     var bn=$("#bname").val();
     var ba=$("#bauthor").val();
     var bp1=$("#bpress").val();
     var bp=$("#bprice").val();
     var bl=$("#floor option:selected").val()+$("#Shelf option:selected").val();
+    var num=$("#cpn").val();
+    tosu();
+    for(var i=0;i<num;i++){
+        $.ajax({
+            async : false,
+            type : "GET",
+            url : "/insertBookWithISBN?ISBN="+isbn+"&price="+bp+"&location="+bl+"&bookname="+bn+"&author="+ba+"&press="+bp1,
+            data : {},
+            success : function(data) {
 
-    $.ajax({
-        async : false,
-        type : "GET",
-        url : "/insertBookWithISBN?ISBN="+isbn+"&price="+bp+"&location="+bl+"&bookname="+bn+"&author="+ba+"&press="+bp1,
-        data : {},
-        success : function(data) {
-            alert("success");
 
-        },
-        error : function(data) {
-            alert("/insertBookWithISBN?ISBN="+isbn+"&price="+bp+"&location="+bl+"&bookname="+bn+"&author="+ba+"&press="+bp1);
-        }
-    });
-};
+            },
+            error : function(data) {
+
+            }
+        });
+    }
+
+}
+
