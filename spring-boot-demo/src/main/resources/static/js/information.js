b759abe5541d2c2c9b076bfcc7e40114a449cefe
justambush <justambush@users.noreply.github.com>
2019-10-14 00:39:41 +0800

    ccy

--- spring-boot-demo/src/main/resources/static/js/information.js
+++ spring-boot-demo/src/main/resources/static/js/information.js
@@ -1,16 +1,30 @@
-function ReadCookie(cookieName)
-{
-    var theCookie=""+document.cookie;
-    var ind=theCookie.indexOf(cookieName);
-    if(ind==-1||cookieName=="")return"";
-    var ind1=theCookie.indexOf(';',ind);
-    if(ind1==-1) ind1=theCookie.length;
-    return unescape(theCookie.substring(ind+cookieName.length+1,ind1));
+function getCookie(cookieName) {
+    //获取所有的cookie "psw=1234we; rememberme=true; user=Annie"
+    var totalCookie = document.cookie;
+    //获取参数所在的位置
+    var cookieStartAt = totalCookie.indexOf(cookieName + "=");
+    //判断参数是否存在 不存在直接返回
+    if (cookieStartAt == -1) {
+        return;
+    }
+    //获取参数值的开始位置
+    var valueStartAt = totalCookie.indexOf("=", cookieStartAt) + 1;
+    //以;来获取参数值的结束位置
+    var valueEndAt = totalCookie.indexOf(";", cookieStartAt);
+    //如果没有;则是最后一位
+    if (valueEndAt == -1) {
+        valueEndAt = totalCookie.length;
+    }
+    //截取参数值的字符串
+    var cookieValue = unescape(totalCookie.substring(valueStartAt, valueEndAt));
+    return cookieValue;
 }
+
+
 function searchInformation() {
 	// var key=$('#id').val();
     //var ide=request.QueryString("idea");
-    var tele=ReadCookie("userids");
+    var tele=getCookie("rtele");
         $.ajax({
             async: false,
             type: "GET",
