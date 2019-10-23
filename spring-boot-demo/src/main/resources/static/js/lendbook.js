ac48a3f90558ffec9a60671187ab2d6085efb449
justambush <justambush@users.noreply.github.com>
2019-10-23 18:45:31 +0800

    ccy

--- spring-boot-demo/src/main/resources/static/js/lendbook.js
+++ spring-boot-demo/src/main/resources/static/js/lendbook.js
@@ -1,12 +1,14 @@
 function lendBook() {
    
       var readerId=$('#name').val();
-      var bookId =$('#email').val();
+      var book1Id =$('#book1').val();
+      var book2Id =$('#book2').val();
+      var book3Id =$('#book3').val();
 
     $.ajax({
         async : false,
         type : "POST",
-        url : "/borrowBook?bookId="+bookId+"&id="+readerId,
+        url : "/borrowBook?bookId="+book1Id+"&id="+readerId,
         data :{},
         success : function(data) {
             alert(data);
@@ -15,6 +17,67 @@ function lendBook() {
                 alert("fail");
         }
     });
+    $.ajax({
+        async : false,
+        type : "POST",
+        url : "/borrowBook?bookId="+book2Id+"&id="+readerId,
+        data :{},
+        success : function(data) {
+            alert(data);
+        },
+        error : function(data) {
+            alert("fail");
+        }
+    });
+    $.ajax({
+        async : false,
+        type : "POST",
+        url : "/borrowBook?bookId="+book3Id+"&id="+readerId,
+        data :{},
+        success : function(data) {
+            alert(data);
+        },
+        error : function(data) {
+            alert("fail");
+        }
+    });
 
+};
 
+function lendBook2() {
+
+    var readerId=$('#name').val();
+    var bookId =$('#book2').val();
+
+    $.ajax({
+        async : true,
+        type : "POST",
+        url : "/borrowBook?bookId="+bookId+"&id="+readerId,
+        data :{},
+        success : function(data) {
+            alert(data);
+        },
+        error : function(data) {
+            alert("fail");
+        }
+    });
+};
+
+function lendBook3() {
+
+    var readerId=$('#name').val();
+    var bookId =$('#book3').val();
+
+    $.ajax({
+        async : true,
+        type : "POST",
+        url : "/borrowBook?bookId="+bookId+"&id="+readerId,
+        data :{},
+        success : function(data) {
+            alert(data);
+        },
+        error : function(data) {
+            alert("fail");
+        }
+    });
 };
