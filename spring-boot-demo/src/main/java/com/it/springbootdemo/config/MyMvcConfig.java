7909c5aedeb41ea0ca5467c6134997620e4924bf
justambush <justambush@users.noreply.github.com>
2019-10-31 14:11:02 +0800

    CCY

--- spring-boot-demo/src/main/java/com/it/springbootdemo/config/MyMvcConfig.java
+++ spring-boot-demo/src/main/java/com/it/springbootdemo/config/MyMvcConfig.java
@@ -52,7 +52,7 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
                 registry.addViewController("/borrow").setViewName("borrow");
                 registry.addViewController("/fineRecord").setViewName("fineRecord");
                 registry.addViewController("/incomeRecord").setViewName("incomeRecord");
-
+                registry.addViewController("/viewDeletionRecord").setViewName("viewDeletionRecord");
                 registry.addViewController("/1").setViewName("1");
                 registry.addViewController("/ViewNews").setViewName("ViewNews");
             }
